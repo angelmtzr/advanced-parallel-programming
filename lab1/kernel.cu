53222218b5b7b24a44ab29b85e2deee0590337c4
angelmtzr <0235347@up.edu.mx>
2024-01-29 09:55:42 -0600

    lab1

---
+++ lab1/kernel.cu
@@ -0,0 +1,16 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void print_hello_cuda()
+{
+	printf("[DEVICE] ThreadIdx.x: %d\n", threadIdx.x);
+}
+
+int main() 
+{
+	print_hello_cuda << <2, 8 >> > ();
+	return 0;
+}
